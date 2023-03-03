c8f68bdc59d5fc3b0e0725ed7fb4da52511e9695
Ajay Sharma <ajay.k.sharma@leidos.com>
2023-03-03 10:19:26 -0500

    first commit

---
+++ root/learn-consul-kubernetes-0.0.10/consul-client-eks/eks.tf
@@ -0,0 +1,37 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.22.0"
+
+  cluster_name    = var.datacenter_name
+  cluster_version = var.kubernetes_version
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups = {
+    first = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 3
+
+      instance_type = var.ec2_instance_type
+    }
+  }
+
+  manage_aws_auth    = false
+  write_kubeconfig   = true
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
---
+++ root/learn-consul-kubernetes-0.0.10/consul-client-eks/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "> 3.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">1.13.3"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = var.default_tags
+  }
+}
